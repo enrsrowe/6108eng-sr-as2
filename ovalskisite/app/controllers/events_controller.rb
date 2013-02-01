0e24a638ce0b24576040a00dbd6885382495c72e
enrsrowe <S.Rowell@2010.ljmu.ac.uk>
2013-02-01 18:01:11 +0000

    Able to view each event.
    Displays all events and can view each event. link to create new but not
    able to create new.

--- ovalskisite/app/controllers/events_controller.rb
+++ ovalskisite/app/controllers/events_controller.rb
@@ -4,5 +4,9 @@ class EventsController < ApplicationController
 		@events = Event.all #@ allows events to be accessed by controller and view.
 	end
 
+	def show
+		@event = Event.find(params[:id])
+	end
+
 
 end
