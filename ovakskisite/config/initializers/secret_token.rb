914ab55934df07a99fd6c236a9fdbceebd0f2310
enrsrowe <S.Rowell@2010.ljmu.ac.uk>
2013-02-01 12:43:25 +0000

    Started a new Rails App

---
+++ ovakskisite/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ovakskisite::Application.config.secret_token = '1378caabd3d06d863d12a09110440557e449576fcd5455b594b91071ffc042184fa073ef6fca20bb2dba3143fcc53adcdcd221ca50b7c78a22423bacbc2a2b50'
