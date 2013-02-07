1781b668d161dfb38f53a95270f4c70a63e4dc01
enrsrowe <S.Rowell@2010.ljmu.ac.uk>
2013-02-07 13:06:42 +0000

    1PM Submit
    Bootstrap Installed
    Page Content Transfered
    3 Controllers
    All working! :D

---
+++ osc/app/controllers/welcome_controller.rb
@@ -0,0 +1,28 @@
+class WelcomeController < ApplicationController
+  def index
+  end
+
+  def getstarted
+  end
+
+  def whatson
+  end
+
+  def aboutus
+  end
+
+  def race
+  end
+
+  def gallery
+  end
+
+  def joinus
+  end
+
+  def access
+  end
+
+  def login
+  end
+end
