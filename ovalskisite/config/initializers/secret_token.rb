c1d43e89a85e9762e77d22e9851d78171fd67c84
enrsrowe <S.Rowell@2010.ljmu.ac.uk>
2013-02-07 18:21:30 +0000

    Deleted old app.
    Deleted the old app ovalskiclub as it is not needed. Programming done
    for today. All done however only thing  left to question is why the
    images dont work on the edit pages and where the back button links to
    after showing a post.

--- ovalskisite/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Ovalskisite::Application.config.secret_token = 'a160c43c7f92c7e29034b25e0087ed53c922e4e486ef135a968bb80a4b187110111f366fe02426ea5c3840c2a0fb2e815f747b2e681f492a5f341316982154d0'
--- ovalskisite/config/routes.rb
+++
@@ -1,7 +0,0 @@
-Ovalskisite::Application.routes.draw do
-  resources :articles
-  resources :events
-  #resources:get "home/index"
-
-  root :to => 'home#index'
-end
